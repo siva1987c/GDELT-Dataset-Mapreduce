41c44f1da3816aa0fcb9e58958b62402f6809ec4
tchira <chyra.tudor@gmail.com>
2015-03-12 15:03:41 +0200

    2 new jobs
    Added monthprotests job with String implementation, monthprotestsmap
    job, with MapWritable implementation

--- src/main/java/com/telenav/hadoop/monthprotests/MonthProtests.java
+++ src/main/java/com/telenav/hadoop/monthprotests/MonthProtests.java
@@ -1,33 +1,36 @@
-package com.telenav.hadoop.influencelistarray;
+package com.telenav.hadoop.monthprotests;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 
+/**
+ * Job: List the number of protests in every country , grouped by month  (or year/month)
+ * 
+ * @author TChira
+ * @version $Revision$
+ */
+public class MonthProtests {
 
-public class InfluenceCountArray {
-
-    public static void main(String[] args) {
-        JobClient client = new JobClient();
-        JobConf conf = new JobConf(InfluenceCountArray.class);
-        
-  
+    public static void main(String[] args){
+        JobClient client=new JobClient();
+        JobConf conf=new JobConf(MonthProtests.class);
         conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(Text.class);
-
+        conf.setOutputValueClass(IntWritable.class);
         // specify input and output dirs
         FileInputFormat.addInputPath(conf, new Path(args[0]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
         // specify a mapper
-        conf.setMapperClass(InfluenceMapperArray.class);
+        conf.setMapperClass(MonthProtestsMapper.class);
 
         // specify a reducer
-        conf.setReducerClass(InfluenceReducerArray.class);
-        //conf.setCombinerClass(InfluenceReducer.class);
+        conf.setReducerClass(MonthProtestsReducer.class);
+        conf.setCombinerClass(MonthProtestsReducer.class);
 
         client.setConf(conf);
         try {
